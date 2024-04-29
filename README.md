110c1fcf91f1de610f2cd44d958b5ba44925d4cb
Cereloran <106718265+Cereloran@users.noreply.github.com>
2024-04-30 07:03:43 +1000

    Added Products to make a total of 11 Different Products

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # CEN207
 
 ## Introduction
+This is a flutter app to address the needs of the CEN207 course.
 
 ### Running the code
+To run this code one can either push the app to their mobile device or use an emulator. To do so, one must have flutter installed on their machine. To install flutter, follow the instructions on the [official flutter website](https://flutter.dev/docs/get-started/install).
+
+
 ```bash
--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class Product {
   Product({
     required this.productName,
     required this.productPrice,
-    this.quantity = 1,
+    this.quantity = 0,
     required this.imagePath,
     required this.description,
 
@@ -49,21 +49,69 @@ class HomePage extends StatefulWidget {
   final List<Product> products = [
     Product(
         productName: 'USB C Cable',
-        productPrice: 10.0,
+        productPrice: 9.99,
         imagePath: 'assets/images/product1.png',
         description: 'This is a USB C cable.'
     ),
     Product(
         productName: 'USB Micro Cable',
-        productPrice: 20.0,
+        productPrice: 19.99,
         imagePath: 'assets/images/product2.png',
         description: 'This is a USB Micro cable.'
     ),
     Product(
         productName: 'USB Mini Cable',
-        productPrice: 30.0,
+        productPrice: 29.99,
         imagePath: 'assets/images/product3.png',
         description: 'This is a USB Mini cable.'
+    ),
+    Product(
+        productName: 'Lightning Cable',
+        productPrice: 14.99,
+        imagePath: 'assets/images/product4.png',
+        description: 'This is a Lightning Charging cable.'
+    ),
+    Product(
+        productName: 'AUX Cable',
+        productPrice: 14.99,
+        imagePath: 'assets/images/product5.png',
+        description: 'This is an AUX cable.'
+    ),
+    Product(
+        productName: 'HDMI Cable',
+        productPrice: 39.99,
+        imagePath: 'assets/images/product6.png',
+        description: 'This is a HDMI cable.'
+    ),
+    Product(
+        productName: 'Display Port Cable',
+        productPrice: 39.99,
+        imagePath: 'assets/images/product7.png',
+        description: 'This is a Display Port cable.'
+    ),
+    Product(
+        productName: 'HDMI Cable',
+        productPrice: 42.99,
+        imagePath: 'assets/images/product8.png',
+        description: 'This is a HDMI cable.'
+    ),
+    Product(
+        productName: 'USB-C to HDMI Cable',
+        productPrice: 27.99,
+        imagePath: 'assets/images/product9.png',
+        description: 'This is a HDMI cable.'
+    ),
+    Product(
+        productName: 'i9 Motherboard Cable',
+        productPrice: 1349.99,
+        imagePath: 'assets/images/product10.png',
+        description: 'Motherboard Micro-Atx DDR4 8GB RAM Intel Core i9 12 13 Gen Gaming Pcie 4.0 PC.'
+    ),
+    Product(
+        productName: 'i9 Cpu',
+        productPrice: 1639.99,
+        imagePath: 'assets/images/product11.png',
+        description: 'Core I9-10900K New I9 10900K 3.7 GHz Ten-Core Twenty-Thread CPU Processor L3=20M 125W LGA 1200.'
     ),
   ];
 
@@ -102,7 +150,6 @@ class _HomePageState extends State<HomePage> {
   Future<void> createAccount() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     await prefs.setBool('isLoggedIn', true);
-    // Add your account creation logic here (e.g., saving username and password)
     setState(() {
       isLoggedIn = true;
     });
